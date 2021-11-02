d91e6391723208b5f62c7acfdb8ebdc80e35a758
Miguel Bennetts <miguel_bennetts@msn.com>
2021-11-01 21:14:39 -0600

    commit message

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,10 +1,10 @@
 pipeline {
     agent any
-    triggers { pollSCM('* * * * *') }
+  
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
+                git url: 'https://github.com/bigmiguel/jgsu-spring-petclinic.git', branch: 'main'
             }            
         }
         stage('Build') {
@@ -16,16 +16,8 @@ pipeline {
             post {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
-                }
-                changed {
-                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                        body: "Please go to ${BUILD_URL} and verify the build", 
-                        attachLog: true, 
-                        compressLog: true, 
-                        to: "test@jenkins",
-                        recipientProviders: [upstreamDevelopers(), requestor()]
-                }
+                    archiveArtifacts 'target/*.jar'                }
+             
             }
         }
     }
